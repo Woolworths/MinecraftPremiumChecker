91df94b97b5255c828b35e6f6633efe632f351f5
Jean Abed <jean.abed@hotmail.com>
2014-07-27 13:28:31 +1000

    first commit :D

---
+++ minecraft.py
@@ -0,0 +1,33 @@
+import urllib2
+
+#while true to make sure we dont need to keep restarting it every time we find the user status
+while True:
+        
+    # gets username, makes sure it's a string and makes it lowercase
+    user = raw_input('>>> = ')
+    user = str(user)
+    user = user.lower()
+    
+    # gets the url
+    url = 'https://www.doctormckay.com/utilities/mc_checker.php?u=' + user
+    
+    # opens url and reads it (and stores it in string)
+    parse = urllib2.urlopen(url)
+    html = parse.read()
+
+    #  this is the part that checks is user is premium or not
+    def runName(name):
+        # if the success label in the html code (if user is premium) then print
+        if '<span class="label label-success">premium</span>' in html:
+            print 'The user' + ' ' + name + ' ' + 'is premium!'
+            
+        # else if success label NOT in html then print
+        elif '<span class="label label-success">premium</span>' not in html:
+            print 'The user' + ' ' + name + ' ' + 'is not premium!'
+
+        # if a strange error has occured
+        else:
+            print 'An error has occured, we could not find the user' + ' ' + name
+
+    # here we just run the function
+    runName(user)
