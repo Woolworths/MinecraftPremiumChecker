55ae269ea94b60c424261d9e40ed9c1c64a2ebe2
Jean Abed <jean.abed@hotmail.com>
2014-07-27 14:30:52 +1000

    use official minecraft username checker (now MUCH faster)

--- minecraft.py
+++ minecraft.py
@@ -1,7 +1,5 @@
 import urllib2
 
-print 'courtesy of doctormckay.com'
-
 #while true to make sure we dont need to keep restarting it every time we find the user status
 while True:
         
@@ -11,7 +9,7 @@ while True:
     user = user.lower()
     
     # gets the url
-    url = 'https://www.doctormckay.com/utilities/mc_checker.php?u=' + user
+    url = 'https://minecraft.net/haspaid.jsp?user=' + user
     
     # opens url and reads it (and stores it in string)
     parse = urllib2.urlopen(url)
@@ -20,11 +18,11 @@ while True:
     #  this is the part that checks is user is premium or not
     def runName(name):
         # if the success label in the html code (if user is premium) then print
-        if '<span class="label label-success">premium</span>' in html:
+        if 'true' in html:
             print 'The user' + ' ' + name + ' ' + 'is premium!'
             
         # else if success label NOT in html then print
-        elif '<span class="label label-success">premium</span>' not in html:
+        elif 'false' in html:
             print 'The user' + ' ' + name + ' ' + 'is not premium!'
 
         # if a strange error has occured
