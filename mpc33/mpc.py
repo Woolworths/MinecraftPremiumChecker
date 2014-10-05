aaab121bd0b79f4bc9a8e181f3712eeb28899cd0
Jean Abed <jean.abed@hotmail.com>
2014-10-05 17:31:08 +1100

    structure overhaul / clarification

--- mpc33/mpc.py
+++ mpc33/mpc.py
@@ -1,27 +1,22 @@
 from urllib.request import urlopen
 
-#while true to make sure we dont need to keep restarting it every time we find the user status
-class Check(object):
-        
-    def __init__(self, user):
-        self.user = user
-            
-        # gets the url
-        url = 'https://minecraft.net/haspaid.jsp?user=%s' % (self.user)
-        
-        # opens url and reads it (and stores it in string)
-        resp = urlopen(url)
-        html = resp.read()
-        self.html = str(html)
+def Check(user):
+    # get the url
+    url = 'https://minecraft.net/haspaid.jsp?user=%s' % (user)
+    
+    # opens url and reads it (and stores it in string)
+    resp = urlopen(url)
+    html = resp.read()
+    html = str(html)
     
     #  this is the part that checks is user is premium or not
-    def run(self):
-        # if the success label in the html code (if user is premium) then print
-        if('true' in self.html): return True
-        # else if success label NOT in html then print
-        elif('false' in self.html): return False
-        # if a strange error has occured
-        else: raise('error')
+
+    # if the success label in the html code (if user is premium) then print
+    if('true' in html): return True
+    # else if success label NOT in html then print
+    elif('false' in html): return False
+    # if a strange error has occured
+    else: raise('error')
         
 def usage():
     print('SEE TESTS.PY FOR EXAMPLES')
--- tests.py
+++ tests.py
@@ -1,13 +1,12 @@
-#for python 3.3:
+# True = user is premium, False = user is not premium
+# for python 3.3:
 '''
-from mpc33.mpc import Check
-app = Check('MinecraftNameInHere')
-print(app.run())
+from mpc33 import Check
+print(Check('MinecraftNameInHere'))
 '''
 
-#for python 2.7:
+# for python 2.7:
 '''
-from mpc27.mpc import Check
-app = Check('MinecraftNameInHere')
-print app.run()
+from mpc27 import Check
+print(Check('MinecraftNameInHere'))
 '''
